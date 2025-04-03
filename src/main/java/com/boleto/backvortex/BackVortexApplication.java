d645ae6ea62ae366f2609db089a568b93f8f2821
Brenno Santos Alves <160975412+brennosantoss@users.noreply.github.com>
2025-04-03 20:18:12 -0300

    arquivo java inicial

---
+++ src/main/java/com/boleto/backvortex/BackVortexApplication.java
@@ -0,0 +1,13 @@
+package com.boleto.backvortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackVortexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackVortexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boleto/backvortex/BackVortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boleto.backvortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackVortexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
